7b2c67fe017e495270b80099e8407539a9369503
Rob Bearman <robbear@hyperfine.com>
2016-02-18 10:03:25 -0800

    Clean up setting of comment text via rwc-comment children

--- public/src/scripts/Comment.js
+++ public/src/scripts/Comment.js
@@ -51,39 +51,36 @@ class Comment extends ElementBase.compose() {
     // to watch for that child to be added at which time we'll capture it as the comment
     // state value, then remove the child element from the DOM.
     //
-    // BUGBUG: Think about how we use entirely virtual dom approaches to address updating
-    // the DOM without having to use parentNode.removeChild. The state of the virtual dom
-    // ideally should cause a full render, eliminating the text child as part of the patch.
-    //
     this.observer = new MutationObserver((mutations) => {
       mutations.forEach((mutation) => {
         // Watch for the addition of any children NOT including the div#comment local dom element.
         // That is, watch for the addition of light dom elements.
-        if (mutation.type == 'childList' && mutation.addedNodes.length > 0 && mutation.addedNodes[0].id !== 'comment') {
+        if (mutation.type == 'childList' &&
+          mutation.addedNodes.length > 0 &&
+          mutation.addedNodes[0].id !== 'comment') {
+
           let node = mutation.addedNodes[0];
+          let commentText = node.textContent;
 
-          // We fetch the textContent of the (assumed) text element child, update state
-          // with the comment, then remove the text element from the DOM. Our
-          // next render will update the virtual DOM, with the DOM patching to follow.
+          // Remove the added nodes now that we've captured the comment. Everything else
+          // is considered a no-op.
+          let nodeList = mutation.addedNodes;
+          for (let i = 0; i < nodeList.length; i++) {
+            this.removeChild(nodeList[i]);
+          }
 
-          // BUGBUG - need action router
-          const action = {
-            type: 'SET_COMMENT',
-            comment: node.textContent
-          };
-
-          // BUGBUG - assumption here is a single TEXT node child
-          node.parentNode.removeChild(node);
-
-          this.store.dispatch(action);
+          this.comment = commentText;
         }
       });
     });
 
+    // Activate the MutationObserver
     this.observer.observe(this, {childList: true});
 
+    // Subscribe to change notifications to the Redux store
     this.store.subscribe(this.storeListener.bind(this));
 
+    // Attach the new local dom to the <rwc-comment> element
     this.appendChild(this.rootNode);
   }
 
@@ -108,6 +105,17 @@ class Comment extends ElementBase.compose() {
   }
   get author() {
     return this.store.getState().author;
+  }
+
+  set comment(commentText) {
+    const action = {
+      type: 'SET_COMMENT',
+      comment: commentText
+    };
+    this.store.dispatch(action);
+  }
+  get comment() {
+    return this.store.getState().comment;
   }
 
   render(state = {}) {
